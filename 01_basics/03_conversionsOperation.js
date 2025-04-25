d67b82a723a8f9266dd6266e1448c718a39739ec
arslanalvi786 <arslanalvi2018@gmail.com>
2025-04-25 13:19:10 +0000

    Conversions of datatypes

---
+++ 01_basics/03_conversionsOperation.js
@@ -0,0 +1,23 @@
+
+let score = "33";
+
+console.log(typeof score)
+console.log(typeof(score));
+
+let valueInNumber = Number(score)
+console.log(typeof valueInNumber);
+
+//  "33"  =>  33
+//  "33abc" =>  NaN
+//  true    =>  1 ;     false   =>  0 ;
+ 
+
+let isLoggedIn = 1
+
+let booleanIsloggedin = Boolean(isLoggedIn);
+console.log(booleanIsloggedin);
+
+
+//  1   =>  true;   0   =>  false
+//  ""  =>  false
+//  "Awan"  =>  true 
