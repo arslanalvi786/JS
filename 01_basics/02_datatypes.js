d71b16d508f86734927bf9ee5eaec1ed7ba97cd8
arslanalvi786 <arslanalvi2018@gmail.com>
2025-04-25 12:48:40 +0000

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,29 @@
+
+"isde strict";  //  treat all code as newer version
+
+// alert( 3 + 3)    //  we are using nodejs , not browser
+
+console.log(3
+    + 3);           //  code readability should be high
+
+console.log("Awan");
+
+
+let name = "Arslan"
+let age = 18
+let isLoggedIn = false
+
+
+//  number => 2 to power 53
+//  bigint
+//  string => "" 
+//  boolean => true/false
+//  null    =>  standalone value
+//  unndefines 
+//  symbol  => unnique
+//  objecct
+
+console.log(typeof "Awan")
+console.log(typeof undefined);      //  undefined
+console.log(typeof null);           //  object
+
