1aa893d3748b66096580b858fe2a0fdd9f6350df
arslanalvi786 <arslanalvi2018@gmail.com>
2025-04-28 13:10:42 +0000

    Stack and Heap

---
+++ 01_basics/stackAndHeap.js
@@ -0,0 +1,24 @@
+
+//  Stack (Premitive)   ,   Heap (Non-Premitive)
+
+let myYTname = "codebyawan"
+let another_name = myYTname
+anothername = "codewithawan"
+
+console.log(myYTname)
+console.log(anothername)
+
+//  Heap :-
+
+let userOne = {
+    email: "awan@gmail.com",
+    id: "awan123",
+}
+
+let userTwo = {
+    email: "user2@gmail.com",
+    id: "user2",
+}
+
+console.log(userOne.email);
+console.log(userTwo.email);
